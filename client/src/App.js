630889581d6ae62ab77ee8d44600f41dab08372a
Jack Frost <albarrankevin97@gmail.com>
2019-12-06 12:25:36 -0800

    added proxy package.json in client and set up test api in server.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [test, setTest] = useState(null);
+
+	useEffect(() => {
+		callTestApi()
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
+
+	const callTestApi = async function() {
+		const res = await fetch("/test");
+		const body = await res.json();
+
+		console.log(res);
+		console.log(body);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5050;
-
+app.get("/test", (req, res) => {
+	res.send({ express: "Express server is up" });
+});
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
 });
