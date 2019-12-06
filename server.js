d6a9dcc3acba327c02baf2f41db6d7f323527a76
Jack Frost <albarrankevin97@gmail.com>
2019-12-06 11:34:13 -0800

    setup express server along with react client

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5050;
+
+app.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
